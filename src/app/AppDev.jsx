d1d6840e1d206698d50c638a4637182ddd78318d
Vladyslav <flickervladislav@gmail.com>
2025-04-30 21:58:38 +0300

    fix: dev base route

--- src/app/AppDev.jsx
+++ src/app/AppDev.jsx
@@ -189,7 +189,7 @@ export function AppDev() {
   }, [data, hasAccess, addLog]);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/testerapp">
       {isLoading ? (
         <Loader height="100vh" />
       ) : (
