d06860d1b96ace50028065e12e7c0af4436485da
Vladyslav <flickervladislav@gmail.com>
2025-07-13 22:14:35 +0300

    fix: route testapp remove const

--- src/app/providers/withRouter.tsx
+++ src/app/providers/withRouter.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ScrollToTop } from '~/shared/router';
 
-const BASE_NAME = '/testapp';
-
 const withRouter = (Component: ComponentType): ComponentType => {
   function WithRouter(): JSX.Element {
     return (
