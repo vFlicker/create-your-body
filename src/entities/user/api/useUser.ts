d664d4f9bdd629edd4e7b5eb839df2102ae7853e
Vladyslav <flickervladislav@gmail.com>
2025-07-14 18:12:42 +0300

    fix: useUser

--- src/entities/user/api/useUser.ts
+++ src/entities/user/api/useUser.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import { userApiService } from './userApiService';
 
-export const useUser = ({ enabled = true }) => {
+export const useUser = ({ enabled = true } = {}) => {
   const { data, isPending } = useQuery({
     queryKey: ['current-user'],
     queryFn: userApiService.getUser,
