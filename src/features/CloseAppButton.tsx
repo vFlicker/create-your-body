dae7aff67f505f67c069245909ccc5858ec1a949
Vladyslav <flickervladislav@gmail.com>
2025-07-22 19:40:23 +0300

    fix: tg back button

--- src/features/CloseAppButton.tsx
+++ src/features/CloseAppButton.tsx
@@ -1,6 +1,9 @@
 import { JSX, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { NavButton } from '~/shared/ui/molecules/buttons/NavButton';
+
+const PREVIOUS_PAGE = -1;
 
 type CloseAppButtonProps = {
   className?: string;
@@ -9,15 +12,20 @@ type CloseAppButtonProps = {
 export function CloseAppButton({
   className,
 }: CloseAppButtonProps): JSX.Element {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const tgBackButton = Telegram.WebApp.BackButton;
     tgBackButton.show();
 
+    const handleBackClick = () => navigate(PREVIOUS_PAGE);
+    tgBackButton.onClick(handleBackClick);
+
     return () => {
-      tgBackButton.offClick(handleClick);
+      tgBackButton.offClick(handleBackClick);
       tgBackButton.hide();
     };
-  }, []);
+  }, [navigate]);
 
   const handleClick = () => {
     Telegram.WebApp.close();
