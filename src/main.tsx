bf8b4c76541fcd09a2d62de7403ea1080ecbaf5d
Vladyslav <flickervladislav@gmail.com>
2025-09-18 15:38:46 +0300

    fix: add more info to error handlers

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import { AppWithProviders } from './app';
+import { userSession } from './shared/libs/userSession';
 import { ErrorBoundary } from './shared/ui/molecules/errorBoundary';
 
 createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,8 @@ window.addEventListener('error', (evt) => {
   fetch('https://cybapp.ru/v2/api/client/log', {
     method: 'POST',
     body: JSON.stringify({
+      type: 'error',
+      token: userSession.getCurrentUser(),
       error: evt.message,
       stack: evt.error?.stack,
       userAgent: navigator.userAgent,
@@ -27,6 +30,8 @@ window.addEventListener('unhandledrejection', (evt) => {
   fetch('https://cybapp.ru/v2/api/client/log', {
     method: 'POST',
     body: JSON.stringify({
+      type: 'unhandledrejection',
+      token: userSession.getCurrentUser(),
       error: evt.reason?.message || 'Unhandled rejection',
       stack: evt.reason?.stack,
       userAgent: navigator.userAgent,
