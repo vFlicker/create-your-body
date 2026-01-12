7d85a1e3b481420863ad819e2daa130fd42313c7
Vladyslav <flickervladislav@gmail.com>
2026-01-12 21:30:44 +0200

    feat: add dashboard redirect for mbi quiz

--- src/features/bmiCalculator/bmiCalculatorQuiz/ui/BmiCalculator.tsx
+++ src/features/bmiCalculator/bmiCalculatorQuiz/ui/BmiCalculator.tsx
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 import { JSX, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { CloseAppButton } from '~/features/CloseAppButton';
 import backIconSrc from '~/shared/assets/svg/back.svg';
+import closeIconSrc from '~/shared/assets/svg/close.svg';
 import { AppRoute } from '~/shared/router';
 import { NavButton } from '~/shared/ui/molecules/buttons/NavButton';
 import { StepProgressBar } from '~/shared/ui/molecules/StepProgressBar';
@@ -27,6 +27,10 @@ export function BmiCalculator(): JSX.Element {
 
   const handleNextClick = () => {
     setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleCloseClick = () => {
+    navigate(AppRoute.Dashboard);
   };
 
   const handleRepeatClick = () => {
@@ -85,7 +89,7 @@ export function BmiCalculator(): JSX.Element {
 
         <StyledPageTitle>Тренажёр по подбору КБЖУ</StyledPageTitle>
 
-        <CloseAppButton />
+        <NavButton iconSrc={closeIconSrc} onClick={handleCloseClick} />
       </StyledHeader>
 
       <StyledContent>
